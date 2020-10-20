6e29e66e3b25f2906171c78a23a52450684166f0
ahh1539 <ahh1539@g.rit.edu>
2020-10-19 20:14:50 -0400

    split dev env into local and prod

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -2,17 +2,9 @@ import os
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
-import sshtunnel
 
 app = Flask(__name__)
-# tunnel = sshtunnel.SSHTunnelForwarder(
-#         ('ssh.pythonanywhere.com'), ssh_username='ahh1539', ssh_password='fvTSYgh$HzB7J23',
-#         remote_bind_address=('ahh1539.mysql.pythonanywhere-services.com', 3306)
-#     )
-# tunnel.start()
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://ahh1539:g3gd@XEbiU7H7Ri@127.0.0.1:{}/ahh1539$pyanywhere_tigerplace'.format(tunnel.local_bind_port)
-app.secret_key = 'super secret key'
-# app.config["DEBUG"] = True
+
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="ahh1539",
     password="g3gd@XEbiU7H7Ri",
@@ -23,9 +15,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['UPLOAD_FOLDER'] = 'mysite/static/'
+app.secret_key = 'super secret key'
+
 db = SQLAlchemy(app)
-
-
 
 class User(db.Model):
     __tablename__ = 'users'
