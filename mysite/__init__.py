983bf4c8d40a47d00120a6efc596f67bd5447b25
Alex Hurley <ahh1539@rit.edu>
2020-10-28 19:55:57 -0400

    testing refactoring

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -7,20 +7,24 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
-tunnel = sshtunnel.SSHTunnelForwarder(
-        ('ssh.pythonanywhere.com'), ssh_username='ahh1539', ssh_password='fvTSYgh$HzB7J23',
-        remote_bind_address=('ahh1539.mysql.pythonanywhere-services.com', 3306)
-    )
-tunnel.start()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://ahh1539:g3gd@XEbiU7H7Ri@127.0.0.1:{}/ahh1539$pyanywhere_tigerplace'.format(tunnel.local_bind_port)
-
+# tunnel = sshtunnel.SSHTunnelForwarder(
+#         ('ssh.pythonanywhere.com'), ssh_username='ahh1539', ssh_password='fvTSYgh$HzB7J23',
+#         remote_bind_address=('ahh1539.mysql.pythonanywhere-services.com', 3306)
+#     )
+# tunnel.start()
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://ahh1539:g3gd@XEbiU7H7Ri@127.0.0.1:{}/ahh1539$pyanywhere_tigerplace'.format(tunnel.local_bind_port)
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="ahh1539",
+    password="g3gd@XEbiU7H7Ri",
+    hostname="ahh1539.mysql.pythonanywhere-services.com",
+    databasename="ahh1539$pyanywhere_tigerplace",
+)
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['UPLOAD_FOLDER'] = 'mysite/static/'
 app.secret_key = 'super secret key'
 
 db = SQLAlchemy(app)
-
 
 
 class User(db.Model):
@@ -33,6 +37,8 @@ class User(db.Model):
     phone_number = db.Column(db.String(50))
     created_at = db.Column(db.DateTime, default=db.func.now())
     deleted_at = db.Column(db.DateTime)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+
 
 class Item(db.Model):
     __tablename__ = 'items'
