c188dad2650cd7642af12b9be4b3a6bd00b3c9cb
Alex Hurley <ahh1539@rit.edu>
2020-10-28 20:43:01 -0400

    massive code cleanup

--- mysite/db.py
+++ mysite/db.py
@@ -1,25 +1,27 @@
-# from mysite import app
-# from mysite import db
+from mysite import db
 
-# class User(db.Model):
-#     __tablename__ = 'users'
-#     user_id = db.Column(db.Integer, db.Sequence('seq_reg_id', start=1, increment=1),
-#               primary_key=True)
-#     email = db.Column(db.String(100))
-#     password = db.Column(db.String(500))
-#     full_name = db.Column(db.String(100))
-#     phone_number = db.Column(db.String(50))
-#     created_at = db.Column(db.DateTime, default=db.func.now())
-#     deleted_at = db.Column(db.DateTime)
 
-# class Item(db.Model):
-#     __tablename__ = 'items'
-#     item_id = db.Column(db.Integer, db.Sequence('seq_reg_id', start=1, increment=1),
-#               primary_key=True)
-#     name = db.Column(db.String(100))
-#     price = db.Column(db.String(100))
-#     description = db.Column(db.String(500))
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-#     img_path = db.Column(db.String(500))
-#     created_at = db.Column(db.DateTime, default=db.func.now())
-#     deleted_at = db.Column(db.DateTime)
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, db.Sequence('seq_reg_id', start=1, increment=1),
+                        primary_key=True)
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(500))
+    full_name = db.Column(db.String(100))
+    phone_number = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    deleted_at = db.Column(db.DateTime)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+
+
+class Item(db.Model):
+    __tablename__ = 'items'
+    item_id = db.Column(db.Integer, db.Sequence('seq_reg_id', start=1, increment=1),
+                        primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.String(100))
+    description = db.Column(db.String(500))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    img_path = db.Column(db.String(500))
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    deleted_at = db.Column(db.DateTime)
