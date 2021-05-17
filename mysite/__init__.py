47060ef4310459dd9af114944d8e6a070497d7e9
ahh1539 <ahh1539@g.rit.edu>
2021-05-17 19:53:33 -0400

    test

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -6,6 +6,22 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 Bootstrap(app)
 
+f=open("mysite/sensitive.txt","r")
+lines=f.readlines()
+username = lines[0].strip('\n')
+ssh_password = lines[1].strip('\n')
+db_password = lines[2].strip('\n')
+db_name = lines[3].strip('\n')
+hostname = lines[4].strip('\n')
+connection_port = int(lines[5].strip('\n'))
+ssh_connection_string = lines[6].strip('\n')
+test_env = bool(lines[7].strip('\n'))
+secret_key = lines[8].strip('\n')
+img_upload_path = lines[9].strip('\n')
+f.close(username, ssh_password, db_password, db_name, hostname, connection_port, ssh_connection_string, test_env, secret_key, img_upload_path)
+
+print()
+
 # for running locally
 # tunnel = sshtunnel.SSHTunnelForwarder(
 #     ('ssh.pythonanywhere.com'), ssh_username='ahh1539', ssh_password='fvTSYgh$HzB7J23',
